de45566a6fb7a8dd94e5ed4ee1dd752b4484df8a
Guilherme Fernandes <guifernandes111@gmail.com>
2023-07-18 18:43:06 -0300

    Add files via upload

---
+++ jogo_forca.py
@@ -0,0 +1,43 @@
+
+import random
+
+a1 = 'python'
+a2 = 'exceto'
+a3 = 'mister'
+a4 = 'script'
+
+lista = [a1, a2, a3, a4]
+
+palavra = random.choice(lista)
+letra_acertada = ''
+repeticoes = 0
+
+while True:
+    letra_digitada = input('Digite uma letra: ')
+        
+    if len(letra_digitada) > 1:
+        print('Digite apenas uma letra')
+        continue
+    if letra_digitada in palavra:
+        letra_acertada += letra_digitada
+
+    palavra_formada = ''
+    for letra_secreta in palavra:
+        if letra_secreta in letra_acertada:
+            palavra_formada += letra_secreta
+        else: 
+            palavra_formada += '*'
+    print('Palavra: ', palavra_formada)  
+
+    repeticoes +=1    
+    
+    if repeticoes >= 15:
+        print('Bonequinho morreu, tente novamente')
+        break
+
+    if palavra_formada == palavra:
+        print('Parabéns você conseguiu salvar a pessoa')
+        print('A palavra era: ', palavra)
+        print('Tentativas: ', repeticoes)
+        break
+
